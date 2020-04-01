1abfaf7d22d3056b27da10c9f806fed46f7fe8d8
wtoy2000 <62914177+wtoy2000@users.noreply.github.com>
2020-03-31 18:35:02 -0600

    Update Store.cpp

--- Store.cpp
+++ Store.cpp
@@ -8,8 +8,9 @@
 #include <fstream>
 #include <cmath>
 using namespace std;
-#include "Companion.h"
-#include "Player.h"
+#include "Store.h"
+
+Store s;
 
 Store::Store()
 {
@@ -18,82 +19,196 @@ Store::Store()
 
 }
 
-void Store::addPlayer(string username)
+void Store::setTotal(double bill_)
 {
-    P.setPlayerName(username);
-    P.setMoney(1000);
-    P.setOxen(0);
-    P.setFood(0);
-    P.setBullets(0);
-    P.setWagonParts(0);
-    P.setMedical(0);
-    
+    bill = bill + bill_;
 }
 
-double Store::buyOxen(int oxenAmount)
+double Store::getTotal()
 {
-    if((oxenAmount * 40) < 100 || (oxenAmount * 40) > 200)
-    {
-        cout<<"Must purchase between $100 and $200 of oxen."<<endl;
-        
-        return;
-    }
-    
-    if(P.getMoney() < (oxenAmount * 40))
-    {
-        cout<<"Not enough money."<<endl;
-        
-        return;
-    }
-    
-    else
-    {
-        P.setOxen(oxenAmount);
-        P.setMoney((oxenAmount * 40));
-        bill = bill + (oxenAmount * 40);
-        cout<<"Total Bill: $"<<bill<<endl;
-    }
-    
+   return bill; 
 }
 
-double Store::buyFood(double foodAmount)
+void Store::shop(Materials &materials1)
 {
+    cout<<"Shop here for materials. Please pick an option:"<<endl;
+    int option;
     
-    if(P.getMoney() < (foodAmount * .5))
+    cout<<"1. Oxen"<<endl;
+    cout<<"2. Food"<<endl;
+    cout<<"3. Bullets"<<endl;
+    cout<<"4. Wagon Parts"<<endl;
+    cout<<"5. Medical Kits"<<endl;
+    cout<<"6. Leave store"<<endl;
+    
+    cin>>option;
+    
+    
+    while (option != 6)
+    
     {
-        cout<<"Not enough money."<<endl;
+        double amount = 0;
+        double total = 0;
         
-        return;
-    }
+        if (option == 1)
+        {
+            cout<<endl;
+            cout<<"You must spend between $100 and $200 on oxen. There are two oxen per yoke. Yokes are $40."<<endl;
+            cout<<"Please enter how many yokes you wish to purchase:"<<endl;
+            cin>>amount;
+            
+            total = 40 * amount;
+            
+            while(amount < 3 || amount > 5)
+            {
+                
+                if(total > materials1.getMoney())
+                {
+                    cout<<"Not enough money."<<endl;
+                }
+                
+                else if(amount < 3 || amount > 5)
+                {
+                    cout<<"Must spend between $100 and $200 on oxen."<<endl;
+                }
+                
+                cout<<"Enter new amount:"<<endl;
+                cin>>amount;
+                total = 40 * amount;
+                
+ 
+            }
+            
+            cout<<"Purchased "<<amount<<" yokes."<<endl;
+            cout<<"You purchased "<<amount * 2<<" oxen."<<endl;
+            cout<<endl;
+            materials1.setOxen(2 * amount);
+            materials1.setMoney(-(total));
+            setTotal(total);
+        }
+        
+        else if(option == 2)
+        {
+            cout<<endl;
+            cout<<"Food is 50 cents a pound, we recommend having 200 pounds per person."<<endl;
+            cout<<"Please enter how many pounds you wish to purchase:"<<endl;
+            cin>>amount;
+            
+            while(amount + materials1.getFoodAmount() > 1000 || amount * .50 > materials1.getMoney())
+            {
+                if(amount * .50 > materials1.getMoney())
+                {
+                    cout<<"You do not have enough money."<<endl;
+                }
+                
+                else if (amount + materials1.getFoodAmount() > 1000)
+                {
+                    cout<<"You cannot hold that much food. You can only hold 1000 pounds."<<endl;
+                }
+                
+                cout<<"Enter a new amount:"<<endl;
+                cin>>amount;
+            }
+            
+            cout<<"You bought "<<amount<<" pounds of food."<<endl;
+            cout<<endl;
+            materials1.setFood(amount);
+            materials1.setMoney(-(.50 * amount));
+            setTotal(.50 * amount);
+        }
+        
+        else if (option == 3)
+        {
+            cout<<endl;
+            cout<<"A box of 20 bullets is $2."<<endl;
+            cout<<"Please enter how many boxes you wish to purchase:"<<endl;
+            cin>>amount;
+            
+            while(amount * 2 > materials1.getMoney())
+            {
+                cout<<"You do not have enough money. Please enter a new amount:"<<endl;
+                cin>>amount;
+            }
+            
+            cout<<"You have purchased "<<amount<<" boxes of bullets."<<endl;
+            cout<<endl;
+            materials1.setBullets(20 * amount);
+            materials1.setMoney(-2 * amount);
+            setTotal(2 * amount);
+        }
+        
+        else if(option == 4)
+        {
+            cout<<endl;
+            cout<<"Wagon parts include wheels, axles, and tongues. Parts are $20."<<endl;
+            cout<<"Please enter how many wagon parts you wish to purchase:"<<endl;
+            cin>>amount;
+            
+            while (amount * 20 > materials1.getMoney())
+            {
+                cout<<"You do not have enough money. Please enter a new amount:"<<endl;
+                cin>>amount;
+            }
+            
+            cout<<"You have purchased "<<amount<<" wagon parts"<<endl;
+            cout<<endl;
+            materials1.setWagonParts(amount);
+            materials1.setMoney(-20 * amount);
+            setTotal(20 * amount);
+        }
+        
+        else if(option == 5)
+        {
+            cout<<endl;
+            cout<<"Medical kits cost $25. Pleas enter how many you wish to purchase."<<endl;
+            cin>>amount;
+            
+            while(amount * 25 > materials1.getMoney())
+            {
+                cout<<"You do not have enough money. Please enter a new amount:"<<endl;
+                cin>>amount;
+            }
+            
+            cout<<"You have purchased "<<amount<<" medical kits"<<endl;
+            cout<<endl;
+            materials1.setMedical(amount);
+            materials1.setMoney(-25 * amount);
+            setTotal(25 * amount);
+            
+        }
+        
+        else
+        {
+            cout<<endl;
+            cout<<"Invalid option"<<endl;
+            cout<<endl;
+        }
+        
+        cout<<"Total bill: $"<<getTotal();
+        cout<<endl;
+        cout<<"You have $"<<materials1.getMoney()<<" in cash."<<endl;
+        cout<<endl;
+        
+        cout<<"Shop here for materials. Please pick an option:"<<endl;
     
-    else
-    {
-        P.setFood(foodAmount);
-        P.setMoney((foodAmount * .50));
-        bill = bill + (foodAmount * .50);
-        cout<<"Total Bill: $"<<bill<<endl;
-    }
-    
-}
+        cout<<"1. Oxen"<<endl;
+        cout<<"2. Food"<<endl;
+        cout<<"3. Bullets"<<endl;
+        cout<<"4. Wagon Parts"<<endl;
+        cout<<"5. Medical Kits"<<endl;
+        cout<<"6. Leave store"<<endl;
+        cout<<endl;
+        
+        cin>>option;
+        
+        
 
-double Store::buyBullets(int bulletsAmount)
-{
-    
-    if(P.getMoney() < (bulletsAmount * 2))
-    {
-        cout<<"Not enough money."<<endl;
-        
-        return;
     }
     
-    else
+    if(option == 6)
     {
-        P.setBullets(bulletsAmount);
-        P.setMoney((bulletsAmount * 2));
-        bill = bill + (bulletsAmount * 2);
-        cout<<"Total Bill: $"<<bill<<endl;
+        cout<<"Thanks for shopping!"<<endl;
     }
-    
 }
 
 
